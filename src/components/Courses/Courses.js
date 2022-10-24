b58b31ada688a9d3b552de3d1e52460c696bc807
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-25 02:09:10 +0600

    course part

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Course from './Course/Course';
 
 const Courses = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/courses')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
+    console.log(courses);
+
     return (
-        <div>
-            this is courses
-        </div>
+        <div className='sm:mx-10 sm:mt-20 mt-3'>
+            <div className='grid sm:grid-cols-2 grid-cols-1'>
+                <div className=' text-white sm:py-4'>
+                    {
+                        courses.map(title => <p
+                            key={title.id} className='sm:my-6'><button className="btn btn-wide"><Link>{title.title}</Link></button></p>)
+
+                    }
+                </div>
+                <div className='grid sm:grid-cols-2'>
+                    {
+                        courses.map(course => <Course key={course.id} course={course}></Course>)
+                    }
+                </div>
+            </div>
+        </div >
     );
 };
 
--- src/components/shared/Header/Header.js
+++ src/components/shared/Header/Header.js
@@ -5,14 +5,15 @@ import navImg from '../../../images/educ.jpg'
 
 const Header = () => {
     return (
-        <div>
+        <div className='sm:mx-10'>
             <div className="navbar bg-base-100">
-                <div className="flex-1">
+                <div className="flex-1 ">
                     <img className='h-8 rounded' src={navImg} alt="" />
-                    <Link className="btn btn-ghost normal-case text-xl">Learnoglance</Link>
+                    <Link className="btn btn-ghost normal-case sm:text-xl">LearnoGlance</Link>
                 </div>
                 <div className="flex-none">
                     <ul className="menu sm:menu-horizontal p-0">
+                        <li><Link to='/'>Home</Link></li>
                         <li><Link to='/courses'>Courses</Link></li>
                         <li><Link>Blog</Link></li>
                         <li><Link>FAQ</Link></li>
