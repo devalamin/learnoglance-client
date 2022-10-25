6e098c0521e7a8b6d0c6d52ac81d330b5cb45617
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-25 23:31:20 +0600

    reload solved

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -9,33 +9,40 @@ const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState();
     const [theme, setTheme] = useState("light");
+    const [loading, setLoading] = useState(true)
 
 
 
 
     const googleSignInProvider = (provider) => {
+        setLoading(true)
         return signInWithPopup(auth, provider)
     }
 
     const githubLoginProvider = (provider) => {
+        setLoading(true)
         return signInWithPopup(auth, provider)
     }
     const createNewUser = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
 
     const LogIn = (email, password) => {
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
 
     }
     const userLogOut = () => {
+        setLoading(true)
         signOut(auth);
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false)
 
         })
         return () => {
@@ -47,7 +54,7 @@ const AuthProvider = ({ children }) => {
         setTheme((curr) => (curr === "light" ? "dark" : "light"));
     };
 
-    const authInfo = { user, toggleTheme, theme, googleSignInProvider, githubLoginProvider, createNewUser, LogIn, userLogOut }
+    const authInfo = { user, loading, toggleTheme, theme, googleSignInProvider, githubLoginProvider, createNewUser, LogIn, userLogOut }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/routes/PrivateRoute/PrivateRoute.js
+++ src/routes/PrivateRoute/PrivateRoute.js
@@ -3,8 +3,16 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+
+    if (loading) {
+        return (
+            <div>
+                <progress className="sm:ml-52 bg-teal-800 text-red-800 progress sm:w-96 w-20"></progress>
+            </div>
+        )
+    }
 
     if (!user) {
         return <Navigate to='/login' state={{ from: location }} replace></Navigate>
