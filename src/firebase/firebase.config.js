df725382753eae57af1f4364ef0db90f9823fe38
devalamin <alamin.hasibmd2017@gmail.com>
2022-10-25 23:50:07 +0600

    environment variables

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDe5jLOC60hp9j-Q6hzQIkcOaxklpcqvH8",
-    authDomain: "learnoglance.firebaseapp.com",
-    projectId: "learnoglance",
-    storageBucket: "learnoglance.appspot.com",
-    messagingSenderId: "260269048299",
-    appId: "1:260269048299:web:21058aa5095064f36f59ae"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
